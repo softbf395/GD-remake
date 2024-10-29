409bb1bdfb0ab3fc2eb9c11ecf7f6064aef9f224
softbf395 <130502864+softbf395@users.noreply.github.com>
2024-10-29 09:26:52 -0500

    Update build_ios_apps.sh

--- build_ios_apps.sh
+++ build_ios_apps.sh
@@ -1,90 +1,10 @@
 #!/bin/bash
 
 # Function to create a minimal project.pbxproj file
-create_pbxproj() {
-  local project_name="$1"
-  local pbxproj_path="$project_name.xcodeproj/project.pbxproj"
-  
-  echo "Creating project.pbxproj for $project_name..."
-  cat <<EOF > "$pbxproj_path"
-// !$*UTF8*$!
-{  
-  archiveVersion = 1;
-  classes = {};
-  objectVersion = 46;
-  objects = {
-    /* Begin PBXProject section */
-    1E0C65F31CFC2289001FAAA9 /* Project object */ = {
-      isa = PBXProject;
-      attributes = {
-        LastUpgradeCheck = 1400;
-        ORGANIZATIONNAME = "RBXRemake Co (family project)"; // Replace with your organization name
-      };
-      buildConfigurationList = 1E0C65F21CFC2289001FAAA9 /* Build configuration list */;
-      compatibilityVersion = "Xcode 15.4";
-      mainGroup = 1E0C65F21CFC2289001FAAA9 /* Source Files */;
-      productRefGroup = 1E0C65F31CFC2289001FAAA9 /* Products */;
-      targets = (
-        1E0C65F21CFC2289001FAAA9 /* Target */;
-      );
-    };
-    /* End PBXProject section */
-    
-    /* Begin PBXNativeTarget section */
-    1E0C65F21CFC2289001FAAA9 /* Target */ = {
-      isa = PBXNativeTarget;
-      buildConfigurationList = 1E0C65F21CFC2289001FAAA9 /* Build configuration list */;
-      dependencies = ();
-      name = "$project_name";
-      productName = "$project_name";
-      productReference = 1E0C65F41CFC2289001FAAA9 /* $project_name */;
-      productType = "com.apple.product-type.application";
-    };
-    /* End PBXNativeTarget section */
+# nvm about ALL that, we can just use something else. brew install xcodegen
 
-    /* Begin PBXFileReference section */
-    1E0C65F41CFC2289001FAAA9 /* $project_name */ = {
-      isa = PBXFileReference;
-      lastKnownFileType = "wrapper.application";
-      path = "$project_name.ipa";
-      sourceTree = "<group>";
-    };
-    /* End PBXFileReference section */
-  };
 
-  rootObject = 1E0C65F31CFC2289001FAAA9 /* Project object */;
-}
-EOF
-}
-
-# Function to add files to the project.pbxproj
-add_files_to_pbxproj() {
-  local project_name="$1"
-  local pbxproj_path="$project_name.xcodeproj/project.pbxproj"
-  local files_dir="$project_name/ios"
-  local file_refs=""
-
-  echo "Adding files from $files_dir to project.pbxproj..."
-  
-  # Iterate over all files in the directory and add them to the pbxproj
-  for file in "$files_dir"/*; do
-    if [ -f "$file" ]; then
-      # Generate a unique identifier for the file
-      local file_id=$(uuidgen | tr '[:upper:]' '[:lower:]')
-      # Add the file entry to the project.pbxproj
-      echo "Adding $file to project.pbxproj"
-      file_refs+="$file_id /* $(basename "$file") */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = \"$(basename "$file")\"; sourceTree = \"<group>\"; };\n"
-    fi
-  done
-  
-  # Append the file references to the pbxproj
-  {
-    echo "/* Begin PBXFileReference section */"
-    echo -e "$file_refs"
-    echo "/* End PBXFileReference section */"
-  } >> "$pbxproj_path"
-}
-
+# Function to add files to the project.pbxproj (UNNEEDED
 # Function to build an iOS app
 build_ios_app() {
   local project_name="$1"
@@ -92,15 +12,16 @@ build_ios_app() {
 
   # Navigate to the iOS folder
   cd "$project_name/ios" || exit
+  xcodegen generate
 
   # Generate Xcode project (if needed)
-  mkdir -p "$project_name.xcodeproj"
+  #mkdir -p "$project_name.xcodeproj"
   
   # Create a minimal project.pbxproj file
-  create_pbxproj "$project_name"
+  #create_pbxproj "$project_name"
 
   # Add raw decompiled code (if necessary)
-  cp -R ../code/* "$project_name.xcodeproj"
+  #cp -R ../code/* "$project_name.xcodeproj"
 
   # Create Info.plist and configure Bundle ID
   echo "Setting up Info.plist..."
@@ -120,7 +41,7 @@ build_ios_app() {
 EOF
 
   # Add files from the ios folder to the project.pbxproj
-  add_files_to_pbxproj "$project_name"
+  #add_files_to_pbxproj "$project_name"
 
   # Build the app without code signing
   echo "Building iOS app $project_name..."
